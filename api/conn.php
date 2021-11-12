fae64f2672df5cf0aefcc99a2d2cbac4d19cc6e3
LoukHin <loukhin3141@gmail.com>
2021-11-13 06:32:34 +0700

    Relocate

---
+++ api/conn.php
@@ -0,0 +1,7 @@
+<?php
+$servername = "node23385-inspired-it.th1.proen.cloud";
+$username = "root";
+$password = "QPHfeg31515";
+$dbname = "popcat";
+    $conn = new mysqli($servername, $username, $password, $dbname);
+?>
