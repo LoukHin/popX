95d517c6ecb9943ea9187720cbae34276e21e664
LoukHin <loukhin3141@gmail.com>
2021-11-13 09:21:12 +0700

    dot id

--- api/updateValue/updateValue.php
+++ api/updateValue/updateValue.php
@@ -5,11 +5,11 @@ require './../conn.php';
 // Takes raw data from the request
 $json = file_get_contents('php://input');
 
-/* 
+/*
  * Convert json to object
  * data should be like this
  * {
- *   "region": "nuea" | "klang" | "esan" | "tai", 
+ *   "region": "nuea" | "klang" | "esan" | "tai",
  *   "add": "0"
  * }
  **/
@@ -21,7 +21,7 @@ if (isset($data->region) && isset($data->add)) {
     $region = $data->region;
     $add = $data->add;
     if (in_array($region, $regions) && $add >= 0 && $add <= 100) {
-        $sql = $conn->prepare("UPDATE `popcat` SET `?` = `?` + ? WHERE `popcat` = 1");
+        $sql = $conn->prepare("UPDATE `popcat` SET `?` = `?` + ? WHERE `popcat`.id = 1");
         $sql->bind_param("ssi", $region, $region, $add);
         $sql->execute();
         $conn->close();
